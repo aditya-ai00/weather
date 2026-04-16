28a1d27f199b4cb26b71c253a967bf8a387b0c70
lp-0406 <laxmiprashasthi@gmail.com>
2026-04-16 15:28:10 +0530

    Update WEATHER_API_KEY in config.example.js

--- config.example.js
+++ config.example.js
@@ -2,6 +2,6 @@
  For Others To See How API_KEYS 
  Are Stored in config.js file*/
 const config = {
-  WEATHER_API_KEY: "PASTE_YOUR_KEY_HERE",
+  WEATHER_API_KEY: "f12945cccd9a4c4ba1194028261604",
 };
 // #NSOC_2k26
