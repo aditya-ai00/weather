59d76fdea40a73a527785d74b311c72f4bbf50bd
Sameer Baral <baralsameer70@gmail.com>
2026-04-20 00:06:28 +0530

    script.js and configue.js updated

--- config.example.js
+++ config.example.js
@@ -2,7 +2,8 @@
  For Others To See How API_KEYS 
  Are Stored in config.js file*/
 const config = {
-  WEATHER_API_KEY: "PASTE_YOUR_KEY_HERE",
+  // WEATHER_API_KEY: "PASTE_YOUR_KEY_HERE",
+  WEATHER_API_KEY: "df6c5d292a62fd09adde6ab9f3135bba",
   
  
 };
