b42fc059c7ea9a5b28d584b25f984dd697b6caad
lamaniaditya275-spec <lamaniaditya275@gmail.com>
2026-04-25 02:17:44 +0530

    Added Up-coming days weather forecast feature with dynamic cards

--- script.js
+++ script.js
@@ -63,6 +63,7 @@ document.getElementById("city").addEventListener("keypress", function(e){
 
 if(e.key === "Enter"){
 getWeather();
+getForecast(e.target.value); // fetch forecast for entered city
 }
 
 });
@@ -76,6 +77,8 @@ function showPosition(position){
 const lat = position.coords.latitude;
 const lon = position.coords.longitude;
 
+ const query = `${lat},${lon}`;
+
 const url =
 `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lat},${lon}`;
 
@@ -84,10 +87,56 @@ fetch(url)
 .then(data => {
 
 updateWeather(data);
-
+getForecast(query); // fetch forecast for current location
 });
 
 }
+
+function updateForecast(data){
+  const forecastContainer = document.getElementById("forecast-conatanir");
+  forecastContainer.innerHTML = ""; // clear previous
+  forecastContainer.innerHTML = "<h3>Up Coming Day Forecast</h3>"; // add header
+  data.forecast.forecastday.forEach((day , index) => {
+        const card = document.createElement("div");
+        card.className = "forecast-card";
+        const date = new Date(day.date);
+        const dayName = date.toLocaleDateString("en-US", { weekday: "short" });
+        
+        let temp;
+
+       if(currentUnit === "C"){
+          temp = Math.round(data.current.temp_c) + " °C";
+       } else {
+          temp = Math.round(data.current.temp_f) + " °F";
+       }
+
+        card.innerHTML = `
+          <h4 class="text-black text-lg font-bold">${dayName}</h4>
+          <p class="text-black text-sm">${day.date}</p>
+          <img src="${day.day.condition.icon}" />
+           <p class="text-black text-sm">  ${temp} - ${day.day.avgtemp_c}°C</p>
+       
+        `;
+        forecastContainer.appendChild(card);
+  });
+}
+
+
+function getForecast(query) {
+  const url =`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${encodeURIComponent(query)}&days=5`;
+
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    // Process and display forecast data
+    console.log(data);
+    updateForecast(data);
+  })
+  .catch(() => {
+    alert("Failed to fetch forecast data");
+  });
+}
+
 
 
 document.getElementById("unit-toggle").addEventListener("click", function(){
